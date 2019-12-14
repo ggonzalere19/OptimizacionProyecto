3e7689776c0d88738d4523cab408c5e243da9519
Gildardo Gonzalez <ggonzalere19@gmail.com>
2019-12-14 01:09:28 -0600

    Problema de signos arreglado

--- visualizer.py
+++ visualizer.py
@@ -3,32 +3,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import scale
 from pandas import DataFrame,Series
-from federatedPCA import merge,privateSAPCA,SMSULQ,SAPCA
 import scipy
 
-dataSetName='wine'
-data = read_csv('./datasets/'+dataSetName+'.csv')
-data=data.drop('quality',axis=1)
-newdf = DataFrame(scale(data), index=data.index, columns=data.columns)
-nunique = newdf.apply(Series.nunique)
-cols_to_drop = nunique[nunique == 1].index
-newdf.drop(cols_to_drop, axis=1)
+# Como algunas componentes pueden tener signos volteados esta funcion sirve para que 
+# las aproximaciones tengan el mismo signo que la original.
+def checkSigns(UOriginal, UAprox):
+    for i in range(UAprox.shape[1]):
+        UAprox[:,i]=np.sign(UOg[0,i]*UAprox[0,i])*UAprox[:,i]
+    return UAprox
 
+# Se selecciona que ejemplo visualizar
+dataSetName='normalData'
+data = read_csv('./datasets/'+dataSetName+'.csv')
+newdf = DataFrame(scale(data), index=data.index, columns=data.columns)
 XMat = newdf.rename_axis('ID').values
 XMat=XMat.T
 
+# Se calcula la svd original para obtener PCA exacto
 UOg, s, VOg = np.linalg.svd(XMat)
 
+# Se leen los valores previamente calculados de las aproximaciones usando SAPCA y private-SAPCA
 UPrivate=np.load('currentUPrivate'+dataSetName+'.npy')
 USapca=np.load('currentU'+dataSetName+'.npy')
 rPrivate=UPrivate.shape[1]
 rSAPCA=USapca.shape[1]
 
+# Se verifica que los signos en todas sean iguales
+USapca=checkSigns(UOg,USapca)
+UPrivate=checkSigns(UOg,UPrivate)
+
+# Se calculan las distancias de Frobenius con respecto de PCA exacto.
+distSapca=np.linalg.norm(UOg[:,:rSAPCA]-USapca)
+distPrivate=np.linalg.norm(UOg[:,:rPrivate]-UPrivate)
+
+# Se evaluan las componentes principales usando las matrices descritas anteriormente
 privateComp=UPrivate.T.dot(XMat)
 ogComp=UOg.T.dot(XMat)
 SapcaComp=USapca.T.dot(XMat)
 
-x=-ogComp[0,:]
+# Se grafican las primeras dos componentes para PCA exacto, SAPCA y private-SAPCA
+x=ogComp[0,:]
 y=ogComp[1,:]
 plt.scatter(x, y)
 plt.xlabel('x')
@@ -36,18 +50,18 @@ plt.ylabel('y')
 plt.title('PCA completo ' + dataSetName)
 plt.show()
 
-x=-SapcaComp[0,:]
-y=-SapcaComp[1,:]
+x=SapcaComp[0,:]
+y=SapcaComp[1,:]
 plt.scatter(x, y)
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('SAPCA '+ dataSetName+' rango: '+ str(rSAPCA))
+plt.title('SAPCA '+ dataSetName+' rango: '+ str(rSAPCA)+ ' dist a U: '+ str(distSapca))
 plt.show()
 
-x=-privateComp[0,:]
+x=privateComp[0,:]
 y=privateComp[1,:]
 plt.scatter(x, y)
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Private SAPCA '+ dataSetName+' rango: '+ str(rPrivate))
+plt.title('Private SAPCA '+ dataSetName+' rango: '+ str(rPrivate)+ ' dist a U: '+ str(distPrivate))
 plt.show()
